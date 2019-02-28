dfd99715158b72c06eb109d48e560c48bbc11dc8
bitebit <kfbygj@gmail.com>
2019-02-28 14:52:20 +0800

    feat(wechat): token store support mongodb

---
+++ wechat/applet/token-store/mongo.go
@@ -0,0 +1,96 @@
+package token
+
+import (
+	"time"
+
+	"github.com/boxgo/box/logger"
+	"github.com/boxgo/kit/mongo"
+)
+
+type (
+	// TokenStore token存储器
+	TokenStore struct {
+		name  string
+		mongo *mongo.Mongo
+
+		originID   string `json:"originId"`
+		DB         string `json:"db"`
+		Collection string `json:"collection"`
+	}
+
+	// wechatToken token db
+	wechatToken struct {
+		ExpireAt time.Time `bson:"expireAt"` // 参数发送时间
+		ExipreIn int       `bson:"exipreIn"` // 过期周期
+		Token    string    `bson:"token"`    // token值
+		OriginID string    `bson:"originId"` // 原始id
+	}
+)
+
+var (
+	// Default 默认的applet token store配置
+	Default = New("applet")
+)
+
+// Name token 配置文件名称
+func (ts *TokenStore) Name() string {
+	return ts.name
+}
+
+// ConfigWillLoad 配置文件将要加载
+func (ts *TokenStore) ConfigWillLoad() {
+
+}
+
+// ConfigDidLoad 配置文件已经加载。做一些默认值设置
+func (ts *TokenStore) ConfigDidLoad() {
+	if ts.originID == "" {
+		panic("originId require config")
+	}
+
+	if ts.mongo == nil {
+		panic("mongo require config")
+	}
+
+	if ts.DB == "" {
+		ts.DB = "applet"
+	}
+
+	if ts.Collection == "" {
+		ts.Collection = "applet_token"
+	}
+}
+
+// Get 获取token
+func (ts *TokenStore) Get() (token string, exipreIn int, expireAt time.Time) {
+	doc := wechatToken{}
+	err := ts.mongo.GetDB(ts.DB).C(ts.Collection).Find(bson.M{"originId": ts.originID}).One(&doc)
+	if err != nil {
+		logger.Default.Errorw("TokenStore.Get.Error", "tokenStore", ts)
+	}
+
+	return doc.Token, doc.ExipreIn, doc.ExpireAt
+}
+
+// Set 保存token
+func (ts *TokenStore) Set(token string, exipreIn int, expireAt time.Time) {
+	doc := wechatToken{
+		Token:    token,
+		ExipreIn: exipreIn,
+		ExpireAt: expireAt,
+		OriginID: ts.originID,
+	}
+	_, err := ts.mongo.GetDB(ts.DB).C(ts.Collection).Upsert(bson.M{"originId": ts.originID}, doc)
+
+	if err != nil {
+		logger.Default.Errorw("TokenStore.Set.Error", "tokenStore", ts)
+	}
+}
+
+// New 新建一个指定名称的tokenstore
+func New(name string) *TokenStore {
+	return &TokenStore{
+		name:  name,
+		mongo: mongo.NewMongo(name),
+	}
+}
