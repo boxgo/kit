43e61ba525aefdf578b13383821b7b95ea5dcdfb
bitebit <kfbygj@gmail.com>
2019-03-04 20:07:33 +0800

    refactor(redislock use redis):

--- schedule/lock/redis.go
+++ schedule/lock/redis.go
@@ -4,21 +4,14 @@ import (
 	"time"
 
 	"github.com/boxgo/box/logger"
-	"github.com/go-redis/redis"
+	"github.com/boxgo/kit/redis"
+	// "github.com/go-redis/redis"
 )
 
 type (
 	// RedisLock redis lock
 	RedisLock struct {
-		MasterName   string   `config:"masterName" desc:"主节点名称，用于哨兵模式"`
-		Address      []string `config:"address" desc:"单节点使用主节点ip；哨兵模式使用哨兵ip列表"`
-		Password     string   `config:"password" desc:"访问秘钥"`
-		DB           int      `config:"db" desc:"数据库索引"`
-		PoolSize     int      `config:"poolSize" desc:"连接池大小"`
-		MinIdleConns int      `config:"minIdleConns" desc:"最小空闲连接"`
-
-		name        string
-		redisClient redis.UniversalClient
+		*redis.Redis
 	}
 )
 
@@ -27,37 +20,15 @@ var (
 	DefaultRedisLock = New("redislock")
 )
 
-// Name 配置文件
-func (l *RedisLock) Name() string {
-	return l.name
-}
-
-// ConfigWillLoad 配置文件将要加载
-func (l *RedisLock) ConfigWillLoad() {
-
-}
-
-// ConfigDidLoad 配置文件已经加载。做一些默认值设置
-func (l *RedisLock) ConfigDidLoad() {
-	l.redisClient = redis.NewUniversalClient(&redis.UniversalOptions{
-		MasterName:   l.MasterName,
-		Addrs:        l.Address,
-		Password:     l.Password,
-		DB:           l.DB,
-		PoolSize:     l.PoolSize,
-		MinIdleConns: l.MinIdleConns,
-	})
-}
-
 // Lock key锁定
 func (l *RedisLock) Lock(key string, ttl time.Duration) (bool, error) {
 	logger.Default.Debugf("redislock.Lock key: %s ttl: %s", key, ttl)
-	return l.redisClient.SetNX(key, 1, ttl).Result()
+	return l.SetNX(key, 1, ttl).Result()
 }
 
 // IsLocked key是否被锁定
 func (l *RedisLock) IsLocked(key string) (bool, error) {
-	result, err := l.redisClient.Get(key).Result()
+	result, err := l.Get(key).Result()
 
 	if err != nil {
 		if err.Error() == "redis: nil" {
@@ -76,7 +47,7 @@ func (l *RedisLock) IsLocked(key string) (bool, error) {
 
 // UnLock 解锁key
 func (l *RedisLock) UnLock(key string) error {
-	_, err := l.redisClient.Del(key).Result()
+	_, err := l.Del(key).Result()
 	logger.Default.Debugf("redislock.UnLock key: %s err: %s", key, err)
 
 	return err
@@ -85,6 +56,6 @@ func (l *RedisLock) UnLock(key string) error {
 // New a redis lock
 func New(name string) *RedisLock {
 	return &RedisLock{
-		name: name,
+		Redis: redis.New(name),
 	}
 }
